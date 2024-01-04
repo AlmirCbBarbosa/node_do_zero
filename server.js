ae3bcb0225dfbac7bce8571f61b6877d5adec67e
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2024-01-04 07:42:31 -0300

    Set host option on listen

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ server.delete('/videos/:id', async (request, reply)=>{ //delete apaga um registr
 
 //comandos finais
 server.listen({
+    host: '0.0.0.0',
     port: process.env.PORT ?? port,
 });
 
