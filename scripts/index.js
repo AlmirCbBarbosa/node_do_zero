3d3367b1b1043b4c5e03908974491ad0107aaf4d
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2024-01-07 13:28:41 -0300

    fecth finalizado, lidar com CORS

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,39 @@ const formulario = document.querySelector('[data-formulario]');
 const entradasFormulario = document.querySelectorAll('[required]');
 
 
-formulario.addEventListener('submit', (e)=>{
+formulario.addEventListener('submit', async e=>{
+    e.preventDefault();
+
+    const videoDados = {
+        "title": e.target.elements["entradaTitulo"].value,
+        "description": e.target.elements["entradaDescricao"].value,
+        "duration": e.target.elements["entradaDuracao"].value
+    };
+
+    try{
+        const response = await fetch('http://localhost:3333/videos', {
+            method: "POST",
+            body: JSON.stringify(videoDados),
+            headers: {"Content-Type": "application/json"}
+        });
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+    }catch (err){
+        console.log(err);
+    }
+
+});
+
+
+
+
+
+
+/* formulario.addEventListener('submit', (e)=>{
     e.preventDefault();  
 
     
@@ -12,6 +44,13 @@ formulario.addEventListener('submit', (e)=>{
         "duration": e.target.elements["entradaDuracao"].value
     }
 
-    console.log(videoDados);
+    fetch(' http://localhost:3333/videos', {
+        method: "POST",
+        body: JSON.stringify(videoDados),
+        headers: {"Content-Type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err=> console.log(err));
     
-})
+}); */
